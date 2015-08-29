b58266c3f1ce35aaadf5a25141597cdeb83760aa
oskosk@gmail.com <oskosk@gmail.com>
2015-08-29 16:13:17 -0300

    merge working

--- config/express.js
+++ config/express.js
@@ -26,7 +26,7 @@ module.exports = function(app, config) {
   app.locals.ENV = env;
   app.locals.ENV_DEVELOPMENT = env == 'development';
 
-  app.set('views', config.root + '/themes/' + config.theme + '/views');
+  app.set('views', [config.root + '/themes/' + config.theme + '/views']);
   app.set('view engine', 'jade');
 
   // app.use(favicon(config.root + '/public/img/favicon.ico'));
